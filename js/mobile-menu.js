68452d2e98c442d6ac4c48c282e3ed34374edf0f
olesyamaniuk <maniukolesya222@gmail.com>
2025-03-04 15:10:40 +0200

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -20,18 +20,12 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   );
 });
-function toggleAnswer(element) {
-  const answer = element.nextElementSibling;
-  const buttonImg = element.querySelector(".toggle-button img"); 
 
-  if (answer.style.display === "none" || answer.style.display === "") {
-    answer.style.display = "block";
-    buttonImg.src = "/img/faq/arrow.png"; 
-  } else {
-    answer.style.display = "none";
-    buttonImg.src = "/img/faq/icon.png"; 
-  }
-}
+document.querySelectorAll(".faq-item").forEach(item => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("flipped");
+  });
+});
 
 
 document.addEventListener("DOMContentLoaded", function () {
